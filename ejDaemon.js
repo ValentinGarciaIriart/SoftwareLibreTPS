96a2a71bb6ce0fa42974845b7abc89884cd9d260
Santiago Escalante <santiesca99@gmail.com>
2023-05-20 14:18:52 -0300

    manejo de errores v2

--- ejDaemon.js
+++ ejDaemon.js
@@ -2,84 +2,96 @@ const http = require('http');
 const fs= require('fs');
 const path = require('path');
 const pathName= '/home/santiagoescalante/SoftwareLibreTPS/clima.txt';
+let tiempoInterval;   //Lo hacemos variable ya que si se excedio el limite de request por dia, deberiamos dormirlo hasta el proximo dia
+
 
 
 //Funcion, get a API de accuweather que trae json con datos del clima por hora de una ciudad especifica, en nuestro caso Mar del Plata
 function getClima(){ 
     const locationKey= '7893';   //Codigo para Mar del Plata, get autocomplete "mar del plata" te devuelve este id
-   
     //queryparams 
     const apiKey='fwooK7kLzbZtlofLaV5pJaRBF5L8DjRe';
     const url= `http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/${locationKey}?apikey=${apiKey}&language=es-ar&details=true&metrics=true`
-   
+    let excede=0;
     //Hago la request
+        const req= http.get(url,(response)=>{
+            let datos='';
    
-    http.get(url,(response)=>{
-        let datos='';
-   
-        //obtengo la data , en chunks
-        response.on('data',(data)=>{
-            datos += data;
-        });
+            //obtengo la data , en chunks
+            response.on('data',(data)=>{
+                datos += data;
+            });
 
-        //Cuando termina de llegar toda la respuesta, parseamos la data,  generamos la linea de contenido y la escribimos en el archivo.
+            //Cuando termina de llegar toda la respuesta, parseamos la data,  generamos la linea de contenido y la escribimos en el archivo.
 
-        response.on('end',()=>{
-            datos= JSON.parse(datos);
-            let celsius= (datos[0].Temperature.Value-32)*(5/9)
-            const contenido = "Hora: "+Date().toString() + " Temperatura: "+celsius.toFixed(1)+"°C"+ " Estado: "+datos[0].IconPhrase+"\n";
-            try{
-                if(fs.existsSync(pathName)){
-                    fs.appendFileSync(pathName,contenido);
+            response.on('end',()=>{
+                datos= JSON.parse(datos);
+                let contenido='';
+                if(datos[0]){    //si no es undefined es porque trajo datos de temperatura
+                    let celsius= (datos[0].Temperature.Value-32)*(5/9)
+                    contenido = "Hora: "+Date().toString() + " Temperatura: "+celsius.toFixed(1)+"°C"+ " Estado: "+datos[0].IconPhrase+"\n";
+                    tiempoInterval=60*60000;
                 }
-                else{
-                    fs.writeFileSync(pathName,contenido);
+                else{   //excedio el limite de requests de nuestra humilde cuenta gratuita
+                    contenido = "el numero permitido de requests ha sido excedido"+"\n";
+                    tiempoInterval=24*60*60000;
                 }
-            }
-            catch(errorEsc){
-                console.log('error al escribir',errorEsc);
-            }
-        })
-
-        response.on('error',(errorReq)=>{
-            if(errorReq.code === 400 ){
-                console.log('la sintaxis no es la adecuada', errorReq);
-            }
-            else if(errorReq.code === 401){
-                console.log('Acceso no autorizado. Fallo de autorizacion de la API',errorReq);
-            }
-            else if(errorReq.code === 403){
-                console.log('Acceso no autorizado. No se tiene los permisos para este endpoint',errorReq);
-            }
-            else if(errorReq.code === 404){
-                console.log('el server no ha encontrado una ruta que coincida con el uri proporcionado',errorReq);
-            }
-            else{   //Error 500
-                console.log('el servidor encontro una condicion inesperada que le impidio cumplir con la solicitud',errorReq);
-                setTimeout(()=>{},24*60*60000);   //Si pase el limite de requests por dia espero 1 dia para volver a hacer request
-            }
-        })
+                try{  //Escribimos en el archivo el contenido
+                    if(fs.existsSync(pathName)){
+                        fs.appendFileSync(pathName,contenido);
+                    }
+                    else{
+                        fs.writeFileSync(pathName,contenido);
+                    }
+                }
+                catch(errorEsc){
+                    console.error('error al escribir',errorEsc);
+                }
+                //Esperamos 1 hora al proximo pronostico o esperamos 1 dia ya que excedimos el limite de requests
+                setTimeout(getClima,tiempoInterval); 
+            })
+            //Errores posibles , luego de estos se reinicia el daemon, ya que configuramos con restart = on-failure
+            response.on('error',(errorRes)=>{
+                if(errorRes.code === 400 ){
+                    console.error('la sintaxis no es la adecuada', errorRes);
+                }
+                else if(errorRes.code === 401){
+                    console.error('Acceso no autorizado. Fallo de autorizacion de la API',errorRes);
+                }
+                else if(errorRes.code === 403){
+                    console.error('Acceso no autorizado. No se tiene los permisos para este endpoint',errorRes);
+                }
+                else if(errorRes.code === 404){
+                    console.error('el server no ha encontrado una ruta que coincida con el uri proporcionado',errorRes);
+                }
+                else{   //Errores 500
+                    console.error('el servidor encontro una condicion inesperada que le impidio cumplir con la solicitud',errorRes);
+                }
+            })
           
-    })
-
+        })
+        //Error de conexion
+        req.on('error',(errorReq)=>{     //Por ejemplo error en la conexion, sin internet
+            console.error("error en la solicitud",errorReq);
+        })
 }
 
 
 
 
 //Primero elimino el archivo existente (no habria que hacerlo ahora)
-fs.unlink(pathName,(errorElim)=>{
-    if (errorElim) {
-        console.error('Error al eliminar el archivo:', errorElim);
-        return;
-      }
-    
-      console.log('El archivo se ha eliminado correctamente.');
-});
-//Intervalos de 1 minuto = 60000 milisegundos
-setInterval(() => {
-  getClima();
-}, 10*1000);  //esta puesto ahora para 10 segundos
+try{
+    fs.unlinkSync(pathName);
+    console.log("El archivo se ha eliminado correctamente");
+}
+catch(errorElim){
+    console.error("Error al eliminar el archivo",errorElim);
+}
+
+//Ejecuto la logica del daemon
+getClima();
+
+
 
 
 
