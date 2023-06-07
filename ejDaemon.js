5fd1cdee9fb1e5e954ec3da56898c7359f8c18c1
Santiago Escalante <santiesca99@gmail.com>
2023-06-07 17:57:42 -0300

    agregado de uso de apis para ip/ubicacion

--- ejDaemon.js
+++ ejDaemon.js
@@ -1,13 +1,19 @@
 const http = require('http');
+const https = require('https');
+const geoip = require('geoip-lite');
+
 let tiempoInterval;   //Lo hacemos variable ya que si se excedio el limite de request por dia, deberiamos dormirlo hasta el proximo dia
 let contenido='';
+//QueryParams
+
+//Codigo de Mar del Plata, lo obtenemos con los request de mas abajo
+let locationKey;
+
+//API KEY Accuweather
+const apiKey='fwooK7kLzbZtlofLaV5pJaRBF5L8DjRe';
 
 //Funcion, get a API de accuweather que trae json con datos del clima por hora de una ciudad especifica, en nuestro caso Mar del Plata
-function getClima(){ 
-     //queryparams 
-    //Codigo para Mar del Plata, get autocomplete "mar del plata" te devuelve este id
-    const locationKey= '7893';   
-    const apiKey='fwooK7kLzbZtlofLaV5pJaRBF5L8DjRe';
+function getClima(){    
     //url
     const url= `http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/${locationKey}?apikey=${apiKey}&language=es-ar&details=true&metrics=true`
     //Hago la request
@@ -25,7 +31,7 @@ function getClima(){
                 if(datos[0]){    
                     let celsius= (datos[0].Temperature.Value-32)*(5/9)
                     contenido = "Fecha: "+Date().toString() + " Temperatura: "+celsius.toFixed(1)+"°C"+ " Estado: "+datos[0].IconPhrase;
-                    tiempoInterval=60*60000;
+                    tiempoInterval=60*60000; //1 hora para el proximo pronostico
                 }
                  //Si se pasa algun parametro mal 
                 else if (datos.Code == 'Unauthorized'){  
@@ -73,5 +79,95 @@ function getClima(){
         })
 }
 
+function getPublicIP(){
+   
+    return new Promise((resolve, reject) => {
+     const req = https.get('https://api.ipify.org?format=json', (res) => {
+        let datos = '';
+
+        res.on('data', (data) => {
+            datos += data;
+        });
+
+        res.on('end', () => {
+        const ip = JSON.parse(datos).ip;
+        resolve(ip);
+        });
+        res.on('error', (errIp) => {
+            reject('error al obtener la ip:'+errIp);
+           });
+     })
+     req.on('error',(errorReq)=>{
+        reject('error al obtener la ip:'+errorReq);
+     })
+  });
+}
+
+function getUbicacion(ipPublica){
+    
+    return new Promise((resolve,reject)=>{
+        const ubicacion = geoip.lookup(ipPublica);
+        resolve(ubicacion);
+    });
+}
+
+function getAutocomplete(ubicacion){
+   
+    const url= `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${ubicacion}&language=es-ar`
+    
+    return new Promise((resolve,reject)=>{
+        const req = http.get(url,(res)=>{
+            let datos='';
+            res.on('data',(data)=>{
+                datos+=data;
+            });
+            res.on('end',()=>{
+                datos=JSON.parse(datos);
+                if(datos[0])
+                 resolve(datos[0].Key);
+                else if (datos.Code == 'Unauthorized'){  
+                    reject('error al obtener el identificador de localizacion:Acceso no autorizado. Fallo de autorizacion de la API',errorRes);    
+                }
+                //excedio el limite de requests de nuestra humilde cuenta gratuita
+                else{   
+                    reject('error al obtener el identificador de localizacion:el numero permitido de requests ha sido excedido');
+                }               
+            });
+            res.on('error',(err)=>{
+                reject('error al obtener el identificador de localizacion:error en la respuesta'+err);
+            });
+        });
+        req.on('error',(erreq)=>{
+            reject('error al obtener el identificador de localizacion:error de conexion'+erreq);
+        })
+    })
+}
+
+let ipPublica;
+let ubicacion;
+
+async function logica() {
+
+    try{
+        if(!ipPublica){ //Si ya hice la requests pero fallo adelante, no vuelvo a ejecutar la funcion
+            ipPublica = await getPublicIP();
+            console.log('IP pública:', ipPublica);
+        }
+        if(!ubicacion && ipPublica){  //En este punto ya tendria la ipPublica pero no la ubicacion
+            ubicacion = await getUbicacion(ipPublica);
+            console.log('ubicacion:',ubicacion.city);
+        }
+        if(!locationKey && ubicacion && ipPublica){ //Tendria la Ip y la ubicacion, me faltaria el id
+            locationKey= await getAutocomplete(ubicacion.city);
+            console.log('locationKey:',locationKey);
+            getClima();
+        }
+    }
+    catch(error){ //Catcheo todos los errores posibles en estas requests y reintento
+        console.error(error);
+        //reintento en 1.5 min
+        setTimeout(logica,90000);
+    }
+}
 //Ejecuto la logica del daemon
-getClima();
+logica();
